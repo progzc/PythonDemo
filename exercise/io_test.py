8f2541926fafbaff6e494d0845a87e3191e0c7bd
progzc <zcprog@foxmail.com>
2021-06-07 00:22:37 +0800

    常用的文件打开模式

--- exercise/io_test.py
+++ exercise/io_test.py
@@ -2,3 +2,20 @@
 file = open('a.txt', 'r', encoding='UTF-8')
 print(file.readlines())
 file.close()
+
+# 写入文件到磁盘
+file = open('b.txt', 'w')
+file.write('Python')
+file.close()
+
+# 追加到文件
+file = open('b.txt', 'a')
+file.write('\ngolang\njava')
+file.close()
+
+# 文件的复制
+src_file = open('a.txt', 'rb')
+target_file = open('copy_a.txt', 'wb')
+target_file.write(src_file.read())
+src_file.close()
+target_file.close()
