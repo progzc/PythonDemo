f590294dfa18d237a38f27d98d5d09add88898ad
progzc <991186676@qq.com>
2021-06-06 18:47:03 +0800

    create convert_test.py

---
+++ exercise/convert_test.py
@@ -0,0 +1,42 @@
+# coding:utf-8
+name = '张三'
+age = 20
+print(type(name), type(age))
+print('我叫' + name + ',今年' + str(age) + '岁')
+
+a = 10
+b = 198.8
+c = False
+print(type(a), type(b), type(c))
+print(str(a), str(b), str(c), type(str(a)), type(str(b)), type(str(c)))
+
+s1 = '128'
+f1 = 98.7
+s2 = '76.77'
+ff = True
+s3 = 'hello'
+print(type(s1), type(f1), type(s2), type(ff), type(s3))
+print(int(s1), type(int(s1)))
+print(int(f1), type(int(f1)))
+# print(int(s2), type(int(s2)))
+print(int(ff), type(int(ff)))
+# print(int(s3), type(int(s3)))
+
+s1 = '128.98'
+s2 = '76'
+ff = True
+s3 = 'hello'
+i = 98
+print(type(s1), type(s2), type(ff), type(s3), type(i))
+print(float(s1), type(float(s1)))
+print(float(s2), type(float(s2)))
+print(float(ff), type(float(ff)))
+# print(float(s3), type(float(s3)))
+print(float(i), type(float(i)))
+
+# 当行注释
+
+'''
+嘿嘿，
+我是多行注释哟
+'''
