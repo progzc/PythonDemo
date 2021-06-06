0e4d82cfa9f9582229e72449d9b03686724dd3be
progzc <991186676@qq.com>
2021-06-06 18:48:32 +0800

    Create type_test.py

---
+++ exercise/type_test.py
@@ -0,0 +1,24 @@
+from decimal import Decimal
+
+i = 26
+print(i, type(i))
+
+a = 3.14159
+print(a, type(a))
+
+print(1.1 + 2.2)
+print(1.1 + 2.1)
+print(Decimal('1.1') + Decimal('2.2'))
+
+f1 = True
+f2 = False
+print(f1, type(f1))
+print(f2, type(f2))
+print(f1+1)
+print(f2+1)
+
+str1 = '人生苦短，我用Python'
+str2 = "人生苦短，我用python"
+str3 = """
+人生苦短，我用python
+"""
